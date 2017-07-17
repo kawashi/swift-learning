dd701afb6f84e78e73af915431eab558acbce7c6
Marihuana <aisatu_nomahou@yahoo.co.jp>
2017-07-18 00:06:08 +0900

    GitHubからユーザ名を取得して表示する機能を追加

--- example2/example2/NextViewController.swift
+++ example2/example2/NextViewController.swift
@@ -8,17 +8,30 @@
 
 import UIKit
 import RealmSwift
+import Alamofire
+import SwiftyJSON
 
 class NextViewController: UIViewController {
 
     @IBOutlet weak var sessionKey: UILabel!
+    @IBOutlet weak var userName: UILabel!
+    
+    @IBAction func getGitHubName(_ sender: Any) {
+        Alamofire.request("https://api.github.com/users/kawashi").responseJSON {response in
+            let data = JSON(response.result.value)
+            self.userName.text = data["name"].string
+            self.userName.isHidden = false
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        userName.isHidden = true
+        
+        // セッションキーを表示している
         let realm = try! Realm()
         let userSession: UserSession? = realm.objects(UserSession).first
-        
         sessionKey.text = userSession?.key
     }
 
@@ -27,3 +40,4 @@ class NextViewController: UIViewController {
     }
     
 }
+
